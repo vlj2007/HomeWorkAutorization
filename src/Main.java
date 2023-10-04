95275b0eef916bb1704070ad821f71aad5c60646
vlj2007 <vlj2007@yandex.ru>
2023-10-05 02:02:09 +0700

    init commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Two Course HomeWork 5");
+        Scanner scanner = new Scanner(System.in);
+        String login = scanner.nextLine();
+        String password = scanner.nextLine();
+        String confirmPassword = scanner.nextLine();
+
+        System.out.println("You login " + login);
+        System.out.println("You password " + password);
+        System.out.println("You password " + confirmPassword);
+
+
+    }
+
+    public static void Authorization(String login, String password, String confirmPassword) {
+
+
+    }
+
+
+}
