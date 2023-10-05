c246dc4065a62a8ae6da973630075342b36d3f76
vlj2007 <vlj2007@yandex.ru>
2023-10-06 00:20:15 +0700

    I have done method Authorization for variable login

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Two Course HomeWork 5");
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your login: ");
         String login = scanner.nextLine();
+        System.out.print("Enter your password: ");
         String password = scanner.nextLine();
+        System.out.print("Enter your password again: ");
         String confirmPassword = scanner.nextLine();
-
         System.out.println("You login " + login);
-        System.out.println("You password " + password);
-        System.out.println("You password " + confirmPassword);
-
-
+        Authorization(login, password, confirmPassword);
     }
 
     public static void Authorization(String login, String password, String confirmPassword) {
-
-
+        if (login.matches("^[a-zA-Z0-9_]{1,20}$")) {
+            System.out.println("Your login " + login + " correct");
+        } else {
+            if ((login == null) || (login.length() == 0) || (!login.trim().isEmpty())) {
+                System.out.println("You have not entered anything");
+                System.out.println(" Your login " + login + " incorrect. Try again.");
+            }
+        }
     }
-
-
 }
