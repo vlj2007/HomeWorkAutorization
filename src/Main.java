2a1af0440030c5a126e3b8ef9ca8e13ed9415163
vlj2007 <vlj2007@yandex.ru>
2023-10-08 01:42:17 +0700

    I have done method authorizationLogin and authorizationPassword. I have create two files WrongLoginException and WrongPasswordException. That was all

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,60 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your login: ");
         String login = scanner.nextLine();
+        try {
+            authorizationLogin(login);
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
         System.out.print("Enter your password again: ");
         String confirmPassword = scanner.nextLine();
-        System.out.println("You login " + login);
-        Authorization(login, password, confirmPassword);
+
+        try {
+            authorizationPassword(password, confirmPassword);
+        } catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("=========Info=============");
+        System.out.println("You login :\t" + login);
+        System.out.println("You password :\t" + password);
+        System.out.println("You confirmPassword :\t" + confirmPassword);
+        System.out.println("==========================");
     }
 
-    public static void Authorization(String login, String password, String confirmPassword) {
-        if (login.matches("^[a-zA-Z0-9_]{1,20}$")) {
-            System.out.println("Your login " + login + " correct");
+    public static void authorizationLogin(String login) throws WrongLoginException {
+        if (login.matches("^[a-zA-Z0-9_]{5,20}$")) {
+            System.out.println("Congratulations! Your login correct");
         } else {
             if ((login == null) || (login.length() == 0) || (!login.trim().isEmpty())) {
-                System.out.println("You have not entered anything");
-                System.out.println(" Your login " + login + " incorrect. Try again.");
+                throw new WrongLoginException("The password must be from 5 to 20 characters");
             }
+            System.out.println(" Your login incorrect. Try again, please.");
         }
+
+
+    }
+
+    public static void authorizationPassword(String password, String confirmPassword) throws WrongPasswordException {
+        if (password.matches("^[a-zA-Z0-9_]{1,20}$")) {
+            System.out.println("Congratulations! The password is correct");
+        } else {
+            if ((password == null) || (password.length() == 0) || (!password.trim().isEmpty()))
+                try {
+                    throw new WrongPasswordException("You did not enter a password");
+                } catch (WrongPasswordException e) {
+                    throw new WrongPasswordException("Passport more than 20 characters");
+                }
+            System.out.println(" Your password incorrect. Try again, please.");
+        }
+
+        if (!password.equals(confirmPassword)){
+            throw new WrongPasswordException("The password and password confirmation fields do not match");
+        }
+
     }
 }
